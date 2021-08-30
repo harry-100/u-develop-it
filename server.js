4bc2e75fe0fff974be7fba002c0204a275f9b826
Harry Shah <harvinder.shah@gmail.com>
2021-08-30 06:53:10 -0400

    server.js updated with db query

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
+
 
 // express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        password: 'Ace@13579',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
 
 // app.get('/', (req, res) => {
 //     res.json({
@@ -12,7 +26,12 @@ app.use(express.json());
 //     });
 // });
 
-//  Default respose for any other request (Not Found)
+db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows);
+    
+});
+
+//  Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
